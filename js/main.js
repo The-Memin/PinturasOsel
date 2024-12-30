3372a50c4ffe13b6a6a76e8355ee6442eedbd8a4
The-Memin <guillejuma@outlook.es>
2024-12-30 11:33:25 -0600

    feat: add page videoteca

---
+++ js/main.js
@@ -0,0 +1,6 @@
+ // Cambiar el video principal al hacer clic en un video pequeño
+ function changeVideo(videoSrc) {
+    const mainVideo = document.getElementById('mainVideo');
+    mainVideo.src = videoSrc;
+    mainVideo.play();
+  }
