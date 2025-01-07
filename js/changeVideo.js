f0afe9047ee35c5b31853646daf63aa5712011cb
Guillermo <guillermo@localhost.localdomain>
2025-01-07 01:21:58 -0600

    feacture: resolve falls

--- js/changeVideo.js
+++ js/changeVideo.js
@@ -1,4 +1,6 @@
 // Cambiar el video principal al hacer clic en un video pequeño
+
+
 function changeVideo(videoSrc) {
     const mainVideo = document.getElementById('mainVideo');
     mainVideo.src = videoSrc;
--- js/componentsLoader.js
+++ js/componentsLoader.js
@@ -1,17 +1,20 @@
 // componentsLoader.js
 
 // Función para cargar un componente
-function loadComponent(component, containerId) {
-    fetch(`components/${component}.html`)
-        .then(response => response.text())
-        .then(data => {
-        // Insertar el componente en el contenedor especificado
-        document.getElementById(containerId).innerHTML = data;
-    })
-        .catch(error => {
-        console.error(`Error loading the component ${component}:`, error);
-    });
-}
- 
-loadComponent('header','header-container');
-loadComponent('footer','footer-container');
+document.addEventListener('DOMContentLoaded', () => {
+    function loadComponent(component, containerId) {
+        fetch(`components/${component}.html`)
+            .then(response => response.text())
+            .then(data => {
+            // Insertar el componente en el contenedor especificado
+            document.getElementById(containerId).innerHTML = data;
+        })
+            .catch(error => {
+            console.error(`Error loading the component ${component}:`, error);
+        });
+    }
+     
+    loadComponent('header','header-container');
+    loadComponent('footer','footer-container');
+  
+})
